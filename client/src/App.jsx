0c0ac8640ac7a672644eeb9db750ab35e981322a
Simultima-qc <nicolas.pelletier@neo6.ca>
2026-09-19 00:48:15 +0100

    Pass tournament start handler to result flow

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -64,6 +64,7 @@ export default function App() {
               <TournamentPage
                 sessionId={sessionId}
                 onEnd={handleTournamentEnd}
+                onStart={handleTournamentStart}
               />
             }
           />
